38e3a236886aa9852ab596f319cf2e790a0005e8
dlope073 <dlope073@fiu.edu>
2016-06-02 18:01:21 -0400

    Hide login functionality when logged in and added logout glyph

--- Code/webapp/features/header/headerDirective.js
+++ Code/webapp/features/header/headerDirective.js
@@ -12,10 +12,10 @@
                 var vm = this;
 				ProfileService.loadProfile().then(function(data){
 					if (data) {
-						vm.user_info = data.firstName;
-						vm.user_type = data.userType;
+						vm.logged_in = true;
 					}
 				});
+				
 				
                 vm.count = 0;
                 ToDoService.loadAllToDo()
