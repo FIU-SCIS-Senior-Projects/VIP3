3bdc2ba572a40c0e8dfa0cc6cb0e24962456bc03
dlope073 <dlope073@fiu.edu>
2016-06-09 14:43:54 -0400

    Fixed where user was granted access before email verification and PI approval

--- Code/api/config/passport.js
+++ Code/api/config/passport.js
@@ -2,6 +2,8 @@ var LocalStrategy = require('passport-local').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/users');
 var configAuth = require('./auth');
+var nodemailer      = require('nodemailer');
+
 module.exports = function(passport) {
    passport.serializeUser(function (user, done) {
        done(null, user.id);
@@ -25,6 +27,9 @@ module.exports = function(passport) {
                 if (!user.comparePassword(password)) {
                     return done(null, false, {message: 'Incorrect username/password.' });
                 }
+				if (!user.verifiedEmail) {
+					return done(null, false, {message: 'Account must be verified' });
+				}
                 return done(null, user);
             });
         }
@@ -47,6 +52,12 @@ module.exports = function(passport) {
                    if (user) {
                        // if a user is found, log them in
                        //console.log('found user' , user);
+					   if (!user.piApproval) {
+						   return done(null, false, {message: 'You must be PI approved.' });
+					   }
+					   if (!user.verifiedEmail) {
+						   return done(null, false, {message: 'You must be PI approved.' });
+					   }
                        return done(null, user);
                    }
                    else {
@@ -65,6 +76,7 @@ module.exports = function(passport) {
 								   newUser.email = profile._json.emails[0].value;
 								   newUser.image = profile._json.image.url;
 								   newUser.google.email = profile.emails[0].value; // pull the first email
+								   newUser.piApproval = false;
 								   // save the user
 								   newUser.save(function (err)
 								   {
@@ -72,6 +84,62 @@ module.exports = function(passport) {
 
 										  console.log(err);
 									   //console.log(newUser);
+									   
+										
+										var recipient = newUser.email;
+										var text = "Dear "+ newUser.firstName +",\n\nWelcome to FIU's VIP Project!"+
+										   " Please verify your email with the link below and standby for your account to be verified by the PI.\n\n http://localhost:3000/vip/verifyEmail/" + newUser.id +"";
+										
+										var subject = "Welcome to FIU VIP Project!";
+
+										var recipient2 = "mtahe006@fiu.edu,dlope073@fiu.edu,vlalo001@fiu.edu"; // NEED TO PUT MAIN PI EMAIL HERE FOR NOW;
+										var text2 = "Dear PI/CoPI,"+
+											" A new user is attempting to register, please accept or reject using the following link:\n\ http://localhost:3000/#/verifyuser/" + newUser.id + "";
+										var subject2 = "User Registration Request";
+
+										var transporter = nodemailer.createTransport({
+											service:'Gmail',
+											auth: {
+												user: 'fiuvipmailer@gmail.com',
+												pass: 'vipadmin123'
+											}
+										});
+
+
+										var mailOptions = {
+											from: 'FIU VIP <vipadmin@fiu.edu>', // sender address
+											to: recipient, // list of receivers
+											subject: subject, // Subject line
+											text: text
+										};
+
+										console.log(mailOptions);
+
+										// send mail with defined transport object
+										transporter.sendMail(mailOptions, function(error, info)
+										{
+											if(error) {
+												return console.log(error);
+											}
+										});
+
+
+										var mailOptions2 = {
+											from: 'FIU VIP <vipadmin@fiu.edu>', // sender address
+											to: recipient2, // list of receivers
+											subject: subject2, // Subject line
+											text: text2
+										};
+
+										console.log(mailOptions2);
+
+										// send mail with defined transport object
+										transporter.sendMail(mailOptions2, function(error, info){
+											if(error) {
+												return console.log(error);
+											}
+										});
+														   
 									   return done(null, newUser);
 
 								   });
