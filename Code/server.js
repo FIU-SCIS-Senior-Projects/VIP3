2176332eac004ca12d39c5b0dea8a5b236bc4424
Vladan Lalovic <vlalo001@fiu.edu>
2016-07-05 20:49:10 -0400

    fixed security/redirects #6

--- Code/server.js
+++ Code/server.js
@@ -14,7 +14,7 @@ var app			= express();
 
 
 //Set HOST 
-app.set("host", "localhost:3000");
+app.set("host", "vip.fiu.edu");
 
 
 //connect to mongodb
@@ -32,6 +32,9 @@ app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
 	next();
 });
 
